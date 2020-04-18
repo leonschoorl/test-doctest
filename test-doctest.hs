2f1267a3c92de01de69dfba31b042a1a0f54200a
quasicomputational <quasicomputational@gmail.com>
2020-04-18 07:37:15 +0100

    Cut the doctest dependency out entirely.

--- test-doctest.hs
+++ test-doctest.hs
@@ -1,5 +1,42 @@
+{-# LANGUAGE CPP #-}
 module Main where
-import Test.DocTest (doctest)
+import Control.Monad ((>=>), void)
+import Control.Monad.IO.Class (liftIO)
+import Digraph (flattenSCCs)
+import DynamicLoading (initializePlugins)
+import DynFlags
+import Exception (ExceptionMonad)
+import GHC
+import GHC.Paths (libdir)
 
 main :: IO ()
-main = doctest ["MyLib.hs"]
+main = void parse
+
+-- Mostly corresponds to https://github.com/sol/doctest/blob/d1b259a2e15f2b27896e01b52bc8cc07edcebc9e/src/Extract.hs
+parse :: IO [TypecheckedModule]
+parse = runGhc (Just libdir) $ do
+  let modules = ["MyLib"]
+
+  modifySessionDynFlags $ \flags ->
+    flags { packageEnv = Just ".ghc.environment.x86_64-linux-8.10.1" }
+
+  mapM (`guessTarget` Nothing) modules >>= setTargets
+  mods <- depanal [] False
+
+  let sortedMods = flattenSCCs (topSortModuleGraph False mods Nothing)
+  reverse <$> mapM (loadModPlugins >=> parseModule >=> typecheckModule >=> loadModule) sortedMods
+  where
+    modifySessionDynFlags :: (DynFlags -> DynFlags) -> Ghc ()
+    modifySessionDynFlags f = do
+      dflags <- getSessionDynFlags
+      -- GHCi 7.7 now uses dynamic linking.
+      let dflags' = case lookup "GHC Dynamic" (compilerInfo dflags) of
+            Just "YES" -> gopt_set dflags Opt_BuildDynamicToo
+            _          -> dflags
+      _ <- setSessionDynFlags (f dflags')
+      return ()
+
+    loadModPlugins modsum = do
+      hsc_env <- getSession
+      dynflags' <- liftIO (initializePlugins hsc_env (GHC.ms_hspp_opts modsum))
+      return $ modsum { ms_hspp_opts = dynflags' }
